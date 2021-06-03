d1fab3a642a47afce8a82a3cc8263b4e8e7fd321
Emily <emily.fitzsimmonsx@gmail.com>
2021-06-03 09:19:31 +0100

    chapter 10

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,4 +1,7 @@
+require_relative 'bike'
+
 class DockingStation
-  def respond_to
+  def release_bike
+  Bike.new
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-require "./lib/dockingstation.rb"
-describe DockingStation do
-end
-=======
 require 'dockingstation.rb'
 describe DockingStation do
-  it 'expects DockingStation instances to respond_to the method release_bike' do
-  respond_to = DockingStation.new
-  respond_to.release_bike
+  it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+  bike = subject.release_bike
+  expect(bike).to be_working
+  end  
 end
-  
-end
->>>>>>> d9d5a6a84b974abba542c443bd7d014ecf68afa8
